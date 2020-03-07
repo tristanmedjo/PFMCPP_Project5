93b3eaf9dca62ed8e5d8bb9afbe542f12d6c6922
matkatmusic <matkatmusic@gmail.com>
2020-03-07 05:25:57 +0000

    Added Part3 Instructions

--- main.cpp
+++ main.cpp
@@ -1,66 +1,43 @@
 /*
-Project 5: Part 2 / 4
- video: Chapter 3 Part 1
+ Project 5: Part 3 / 4
+ video: Chapter 3 Part 4: 
 
-Create a branch named Part2
+Create a branch named Part3
 
- The 'this' keyword
+ the 'new' keyword
+
+ 1) add #include "LeakedObjectDetector.h" to main
  
- 1) if you don't have any std::cout statements in main() that access member variables of your U.D.Ts
-         write some. 
+ 3) Add 'JUCE_LEAK_DETECTOR(OwnerClass)' at the end of your UDTs.
  
- 2) For each std::cout statement in main() that accessed member variables of your types or printed out the results of your member function calls,
-        a) write a member function that prints the same thing out, but uses the proper techniques inside the member functions to access the same member variables/functions.
-        b) be explicit with your use of 'this->' in those member functions so we see how you're accessing/calling those member variables and functions *inside*
-        c) call that member function after your std::cout statement in main.
+ 4) write the name of your class where it says "OwnerClass"
  
- 3) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
+ 5) write wrapper classes for each type similar to how it was shown in the video
+ 
+ 7) update main to use your wrapper classes, which have your UDTs as pointer member variables.
+ 
+ 8) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
  
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
- 
+
 Make a pull request after you make your first commit and pin the pull request link to our DM thread.
 
 send me a DM to check your pull request
 
  Wait for my code review.
+ 
+ see here for an example: https://repl.it/@matkatmusic/ch3p04example
  */
 
-/*
- example:
- */
-#include <iostream>
-namespace Example
-{
-    //a User-Defined Type
-    struct MyFoo
-    {
-        MyFoo() { std::cout << "creating MyFoo" << std::endl; }
-        ~MyFoo() { std::cout << "destroying MyFoo" << std::endl; }
-		
-		// 2a) the member function whose function body is almost identical to the std::cout statement in main.
-        void memberFunc() 
-		{ 
-            // 2b) explicitly using 'this' inside this member function.
-			std::cout << "MyFoo returnValue(): " << this->returnValue() << " and MyFoo memberVariable: " << this->memberVariable << std::endl; 
-		}  
-		
-        int returnValue() { return 3; }
-        float memberVariable = 3.14f;
-    };
-    
-    int main()
-    {
-        //an instance of the User-Defined Type named mf
-        MyFoo mf;
-		
-        // 1) a std::cout statement that uses mf's member variables
-        std::cout << "mf returnValue(): " << mf.returnValue() << " and mf memberVariable: " << mf.memberVariable << std::endl; 
-		
-        // 2c) calling mf's member function.  the member function's body is almost identical to the cout statement above.
-        mf.memberFunc();
-        return 0;
-    }
-}
+
+
+
+
+
+
+
+
+
 
 
 #include <iostream>
